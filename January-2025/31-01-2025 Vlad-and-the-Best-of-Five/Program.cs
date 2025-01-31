ea05177ddd85ca27e48751850661e4a5c24bbbf0
Tanvir Ahmed <TAhmed@rensafamily.company>
2025-01-31 23:56:36 +0600

    31-01-2025 "Vlad-and-the-Best-of-Five"

---
+++ January-2025/31-01-2025 Vlad-and-the-Best-of-Five/Program.cs
@@ -0,0 +1,18 @@
+﻿var input = Convert.ToInt16(Console.ReadLine());
+
+for (byte i = 0; i < input; i++)
+{
+    string inputString = Console.ReadLine();
+
+    int countA = inputString.Count(c => c == 'A');
+    int countB = 5 - countA;
+
+    if (countA > countB)
+    {
+        Console.WriteLine("A");
+    }
+    else
+    {
+        Console.WriteLine("B");
+    }
+}
