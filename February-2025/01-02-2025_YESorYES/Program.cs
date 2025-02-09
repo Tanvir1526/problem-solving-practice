59f9fab9ba15b7b96396bfaeb9f259601d7043e6
tanvir1526 <astro.tanvir70@gmail.com>
2025-02-09 12:24:31 +0600

    01-02-2025_YESorYES

---
+++ February-2025/01-02-2025_YESorYES/Program.cs
@@ -0,0 +1,19 @@
+﻿int input = Convert.ToInt32(Console.ReadLine());
+
+static bool CheckYes(string? inputChar)
+{
+    return inputChar != null && inputChar.Equals("yes", StringComparison.OrdinalIgnoreCase);
+}
+
+for (int i = 0; i < input; i++)
+{
+    var inputChar = Console.ReadLine();
+    if (CheckYes(inputChar))
+    {
+        Console.WriteLine("YES");
+    }
+    else
+    {
+        Console.WriteLine("NO");
+    }
+}
